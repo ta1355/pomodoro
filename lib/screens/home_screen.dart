df16fb84940c31f59bc518a1f18a564b65d37678
yoo <eorbs255@naver.com>
2025-07-23 19:23:18 +0900

    타이머 시간 포메팅(MM:SS)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,17 +10,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<HomeScreen> {
-  int totalSeconds = 1500;
+  static const int minute = 1500;
+
+  int totalSeconds = minute;
+
   late Timer timer;
 
+  bool isRunning = false;
+
+  int totalPomodoros = 0;
+
   void onTick(Timer timer) {
-    setState(() {
-      totalSeconds = totalSeconds - 1;
-    });
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros += 1;
+        isRunning = false;
+        totalSeconds = minute;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
   }
 
   void onStartPressed() {
     timer = Timer.periodic(Duration(seconds: 1), onTick);
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split(".").first.substring(2, 7);
   }
 
   @override
@@ -34,7 +65,7 @@ class _MyWidgetState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "$totalSeconds",
+                format(totalSeconds),
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 120,
@@ -49,8 +80,12 @@ class _MyWidgetState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 98,
                 color: Theme.of(context).cardColor,
-                onPressed: onStartPressed,
-                icon: Icon(Icons.play_circle_fill_outlined),
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                icon: Icon(
+                  isRunning
+                      ? Icons.pause_circle_filled_outlined
+                      : Icons.play_circle_fill_outlined,
+                ),
               ),
             ),
           ),
@@ -78,7 +113,7 @@ class _MyWidgetState extends State<HomeScreen> {
                           ),
                         ),
                         Text(
-                          '0',
+                          '$totalPomodoros',
                           style: TextStyle(
                             fontSize: 58,
                             fontWeight: FontWeight.w600,
